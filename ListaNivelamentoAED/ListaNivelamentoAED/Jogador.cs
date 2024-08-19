93d42694aa8e543ae1c748313278aff0466a0e6b
Matheus Filipe de Deus <161864558+0matheusfilipe@users.noreply.github.com>
2024-08-19 16:59:20 -0300

    Add files via upload

---
+++ ListaNivelamentoAED/ListaNivelamentoAED/Jogador.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaNivelamentoAED
+{
+    internal class Jogador
+    {
+        private int _numero;
+        private string _nome;
+        private string _posicao;
+
+        public int Numero { get { return _numero; } set { _numero = value; } }
+        public string Nome { get { return _nome; } set { _nome = value; } }
+        public string Posicao { get { return _posicao; } set { _posicao = value; } }
+
+        public Jogador(int numero, string nome, string posicao)
+        {
+            this._numero = numero;
+            this._nome = nome;
+            this._posicao = posicao;
+        }
+
+    }
+}
+
+
---
+++ ListaNivelamentoAED/ListaNivelamentoAED/Time.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaNivelamentoAED
+{
+    internal class Time
+    {
+        private string _nome;
+        private Jogador[] _titulares;
+        private int _quantTitulares;
+        private Jogador[] _reservas;
+        private int _quantReservas;
+
+        public string Nome { get { return _nome; } set { _nome = value; } }
+
+        public Time()
+        {
+            _titulares = new Jogador[11];
+            _reservas = new Jogador[12];
+            _quantReservas = 0;
+            _quantTitulares = 0;
+        }
+
+    }
+}
