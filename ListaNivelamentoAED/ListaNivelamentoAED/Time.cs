f0878e5da61371bb7847d15c202e422a0d07f829
Matheus Filipe de Deus <161864558+0matheusfilipe@users.noreply.github.com>
2024-08-20 16:58:21 -0300

    Update Time.cs

--- ListaNivelamentoAED/ListaNivelamentoAED/Time.cs
+++ ListaNivelamentoAED/ListaNivelamentoAED/Time.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,20 +8,57 @@ namespace ListaNivelamentoAED
 {
     internal class Time
     {
-        private string _nome;
-        private Jogador[] _titulares;
-        private int _quantTitulares;
-        private Jogador[] _reservas;
-        private int _quantReservas;
+        public string Nome { get; set; }
+        public Jogador[] Titulares { get; set; }
+        public int QuantTitulares { get; set; }
+        public Jogador[] Reservas { get; set; }
+        public int QuantReservas { get; set; }
 
-        public string Nome { get { return _nome; } set { _nome = value; } }
-
-        public Time()
+        public Time(string nome)
         {
-            _titulares = new Jogador[11];
-            _reservas = new Jogador[12];
-            _quantReservas = 0;
-            _quantTitulares = 0;
+            Nome = nome;
+            Titulares = new Jogador[11];
+            Reservas = new Jogador[12];
+            QuantTitulares = 0;
+            QuantReservas = 0;
+        }
+
+        public bool AdicionarTitular(Jogador jogador)
+        {
+            if(QuantTitulares < 12)
+            {
+                Titulares[QuantTitulares] = jogador;
+                QuantTitulares++;
+                Console.WriteLine($"Jogador {jogador.Nome} adicionado aos titulares.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Não é possível adicionar jogadores porque já existem 11 titulares.");
+                return false;
+            }
+
+        }
+
+        public bool AdicionarReserva(Jogador jogador)
+        {
+            if (QuantReservas < 13)
+            {
+                Reservas[QuantReservas] = jogador;
+                QuantReservas++;
+                Console.WriteLine($"Jogador {jogador.Nome} adicionado aos reservas.");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"Não foi possível jogadores na reserva.");
+                return false;
+            }
+        }
+
+        public bool SubstituirTitular(string nome, Jogador novoJogador)
+        {
+            if(Titulares)
         }
 
     }
