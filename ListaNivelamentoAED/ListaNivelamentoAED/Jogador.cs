adc14d1796e55c7e7f8a792606869f195dd26a19
Matheus Filipe de Deus <161864558+0matheusfilipe@users.noreply.github.com>
2024-08-20 16:58:35 -0300

    Update Jogador.cs

--- ListaNivelamentoAED/ListaNivelamentoAED/Jogador.cs
+++ ListaNivelamentoAED/ListaNivelamentoAED/Jogador.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,19 +9,15 @@ namespace ListaNivelamentoAED
 {
     internal class Jogador
     {
-        private int _numero;
-        private string _nome;
-        private string _posicao;
-
-        public int Numero { get { return _numero; } set { _numero = value; } }
-        public string Nome { get { return _nome; } set { _nome = value; } }
-        public string Posicao { get { return _posicao; } set { _posicao = value; } }
+        public int Numero { get; set; }
+        public string Nome { get; set; }
+        public string Posicao { get; set; }
 
         public Jogador(int numero, string nome, string posicao)
         {
-            this._numero = numero;
-            this._nome = nome;
-            this._posicao = posicao;
+            Numero = numero;
+            Nome = nome;
+            Posicao = posicao;
         }
 
     }
